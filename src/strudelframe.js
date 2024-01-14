2849c470894741b5c4e4607a82c4fd4dcc2cb2a5
Jade (Rose) Rowland <jaderowlanddev@gmail.com>
2024-01-13 22:16:02 -0500

    trying to get data out of iframe

--- src/strudelframe.js
+++ src/strudelframe.js
@@ -1,8 +1,17 @@
+import { useRef } from "react";
 import "./App.css";
 
-export function StrudelFrame() {
+export function StrudelFrame({}) {
+  const ref = useRef();
+
+  // setInterval(() => {
+  //   console.log(ref?.current?.contentWindow?.location?.href);
+  // }, 1000);
+  // const source = "https://hydra.ojack.xyz";
+  const source = "http://localhost:4321/";
   return (
     <iframe
+      ref={ref}
       style={{
         display: "flex",
         flexGrow: 1,
@@ -11,7 +20,7 @@ export function StrudelFrame() {
         border: "0px",
       }}
       title="strudel embed"
-      src="http://localhost:4321/"
+      src={source}
     ></iframe>
   );
 }
